60abc99058ef411ac85bcebf2176b94725d22d8b
李志 <lizhi2@8531.cn>
2018-05-14 13:12:57 +0800

    增加自动生成的初始化titleView的代码

--- app/src/main/java/com/zjonline/xsbprocessor/MainActivity.java
+++ app/src/main/java/com/zjonline/xsbprocessor/MainActivity.java
@@ -8,13 +8,17 @@ import android.view.View;
 import com.zjonline.lib_annotation.LayoutAnn;
 import com.zjonline.lib_annotation.LayoutAnnInit;
 
-@LayoutAnn(layout = R.layout.activity_main, title = "你好")
+@LayoutAnn(layout = R.layout.activity_main, titleStringRes = R.string.hello, rightImgRes = {R.mipmap.ic_launcher_round, R.mipmap.ic_launcher_round})
 public class MainActivity extends AppCompatActivity {
-
+TitleView titleView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
         LayoutAnnInit.bind(this, R.id.xsb_view_title);
+//        titleView = findViewById(R.id.xsb_view_title);
+//        titleView.setRightOneImge(R.mipmap.ic_launcher_round);
+//        titleView.setRightTwoImge(R.mipmap.ic_launcher_round);
 
     }
 
